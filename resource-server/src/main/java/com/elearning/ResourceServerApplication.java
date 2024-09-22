45089c1dbcc1f310cbf84ae1fed560c76f317a3e
thainguyencoffee <nguyennt11032004@gmail.com>
2024-09-22 20:18:51 +0700

    remove EnableJdbcRepositories to fix error: Error creating bean with name 'discountRepository' defined in com.elearning.discount.domain.DiscountRepository defined in @EnableJdbcRepositories declared on ResourceServerApplication: Cannot resolve reference to bean 'org.springframework.data.jdbc.core.mapping.JdbcMappingContext' while setting bean property 'mappingContext' && Caused by: org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'org.springframework.data.jdbc.core.mapping.JdbcMappingContext' available

--- resource-server/src/main/java/com/elearning/ResourceServerApplication.java
+++ resource-server/src/main/java/com/elearning/ResourceServerApplication.java
@@ -2,10 +2,8 @@ package com.elearning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
-@EnableJdbcRepositories
 public class ResourceServerApplication {
 
     public static void main(String[] args) {
