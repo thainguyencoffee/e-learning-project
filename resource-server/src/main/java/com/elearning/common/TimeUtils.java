225af2d72f5947affacdc5899802fe43a34c4695
thainguyencoffee <nguyennt11032004@gmail.com>
2024-10-04 09:20:57 +0700

    patch

---
+++ resource-server/src/main/java/com/elearning/common/TimeUtils.java
@@ -0,0 +1,12 @@
+package com.elearning.common;
+
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public interface TimeUtils {
+
+    static final DateTimeFormatter FORMATTER = DateTimeFormatter
+            .ofPattern("yyyy-MM-dd HH:mm:ss")
+            .withZone(ZoneId.systemDefault());
+
+}
