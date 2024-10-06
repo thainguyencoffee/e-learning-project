7aff1d436ab79e83ff44f066f1f19d12f8394bbc
Thái Nguyên <119722088+thainguyencoffee@users.noreply.github.com>
2024-10-06 16:54:26 +0700

    fix edge-service's error: no csrf-token sent to browser (#30)
    * fix edge-service's error: no csrf-token sent to browser
    
    * require-action when find changed in edge-service

--- edge-service/src/main/java/com/elearning/edgeservice/config/SecurityConfig.java
+++ edge-service/src/main/java/com/elearning/edgeservice/config/SecurityConfig.java
@@ -1,26 +1,24 @@
 package com.elearning.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.server.ServerOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.server.WebSessionServerOAuth2AuthorizedClientRepository;
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
-import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
+import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
 @EnableWebFluxSecurity
-@Configuration
 public class SecurityConfig {
 
     @Bean
@@ -39,7 +37,7 @@ public class SecurityConfig {
                 )
                 .exceptionHandling(exceptionHandling -> exceptionHandling
                         .authenticationEntryPoint(new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .oauth2Login(oauth2Login -> oauth2Login.authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/spa")))
+                .oauth2Login(Customizer.withDefaults())
                 .logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler(clientRegistrationRepository)))
                 .csrf(csrf -> csrf.csrfTokenRepository(CookieServerCsrfTokenRepository.withHttpOnlyFalse()))
                 .build();
@@ -56,7 +54,7 @@ public class SecurityConfig {
         // Required because of https://github.com/spring-projects/spring-security/issues/5766
         return (exchange, chain) -> {
             exchange.getResponse().beforeCommit(() -> Mono.defer(() -> {
-                Mono<CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
+                Mono<org.springframework.security.web.server.csrf.CsrfToken> csrfToken = exchange.getAttribute(CsrfToken.class.getName());
                 return csrfToken != null ? csrfToken.then() : Mono.empty();
             }));
             return chain.filter(exchange);
