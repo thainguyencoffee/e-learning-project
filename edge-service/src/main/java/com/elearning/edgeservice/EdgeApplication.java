4cf077c17fcf4b62d991039abceb394c016cb52d
thainguyencoffee <nguyennt11032004@gmail.com>
2024-10-06 16:41:19 +0700

    fix edge-service's error: no csrf-token sent to browser

--- edge-service/src/main/java/com/elearning/edgeservice/EdgeApplication.java
+++ edge-service/src/main/java/com/elearning/edgeservice/EdgeApplication.java
@@ -2,8 +2,9 @@ package com.elearning.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
 public class EdgeApplication {
 
 	public static void main(String[] args) {
