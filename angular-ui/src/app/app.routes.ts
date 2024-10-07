a4e2b7c361887e8c27a164808cd074ab8519cd45
Thái Nguyên <119722088+thainguyencoffee@users.noreply.github.com>
2024-10-07 18:05:17 +0700

    add (#32)
    * add
    
    * fix github-ci

--- angular-ui/src/app/app.routes.ts
+++ angular-ui/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from "./home/home.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    // title, path, component
+    title: 'Welcome to E learning!',
+    path: '',
+    component: HomeComponent
+  }
+];
