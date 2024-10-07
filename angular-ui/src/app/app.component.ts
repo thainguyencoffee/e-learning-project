86f28bf39352fcadca8da5c92172c2e8a71cebd0
thainguyencoffee <nguyennt11032004@gmail.com>
2024-10-07 17:57:50 +0700

    add

--- angular-ui/src/app/app.component.ts
+++ angular-ui/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HeaderComponent} from "./common/header/header.component";
+import {FooterComponent} from "./common/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
